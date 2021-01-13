df6d76bfec8dc7a2744daa85eda7ac4c46a66b5a
tomas agnese <tagnese1985@gmail.com>
2021-01-12 22:28:00 -0300

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(
+    name:    Faker::Restaurant.name,
+		address: 	Faker::Address.street_address,
+		phone_number: Faker::PhoneNumber.cell_phone,
+		category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+	)
+end
